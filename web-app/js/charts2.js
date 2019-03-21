45db53f6f0e914b839371ad21f2a46ba51d3b6be
Reuben Roberts <r.roberts@nbn.org.uk>
2019-03-21 07:18:35 +0000

    Bugfix for species link

--- web-app/js/charts2.js
+++ web-app/js/charts2.js
@@ -1249,7 +1249,7 @@ function showBie(node) {
     var rank = node.attr('rank');
     if (rank == 'kingdoms') return;
     var name = node.attr('id');
-    var sppUrl = "https://bie.ala.org.au/species/" + name;
+    var sppUrl = CHARTS_CONFIG.bieWebappUrl + "/species/" + name;
     if (rank != 'species') { sppUrl += "_(" + rank + ")"; }
     document.location.href = sppUrl;
 }
